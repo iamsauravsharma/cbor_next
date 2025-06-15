c21ce9dfaf0b388ff349d97e9039c4fe7dcb7969
iamsauravsharma <appdroiddeveloper@gmail.com>
2025-06-15 02:23:19 +0000

    Deploying to gh-pages from @ iamsauravsharma/cbor_next@e784472717df7e8f5274b4bbe4ea37e151a5c86f 🚀

--- trait.impl/core/cmp/trait.Ord.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["cbor_next",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"cbor_next/data_item/enum.DataItem.html\" title=\"enum cbor_next::data_item::DataItem\">DataItem</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[278]}
--- trait.impl/core/cmp/trait.PartialOrd.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["cbor_next",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"cbor_next/data_item/enum.DataItem.html\" title=\"enum cbor_next::data_item::DataItem\">DataItem</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[299]}
