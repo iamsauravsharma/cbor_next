441ef34e5724d14c05ab683d368239df68e2856d
Saurav Sharma <appdroiddeveloper@gmail.com>
2025-06-16 09:15:29 +0545

    refactor: use own error implementation
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,26 +1,58 @@
+use std::num::TryFromIntError;
+use std::string::FromUtf8Error;
+
 /// Enum representing error for a crate
-#[derive(thiserror::Error, PartialEq, Eq, Debug)]
+#[derive(Debug, PartialEq)]
 #[non_exhaustive]
 pub enum Error {
     /// Incomplete CBOR bytes
-    #[error("Incomplete CBOR bytes")]
+    // #[error("Incomplete CBOR bytes")]
     Incomplete,
     /// Error generated when converted string from utf8 bytes
-    #[error(transparent)]
-    FromUtf8(#[from] std::string::FromUtf8Error),
+    FromUtf8(FromUtf8Error),
     /// Incomplete indefinite length data
-    #[error("incomplete indefinite length data")]
     IncompleteIndefinite,
     /// Invalid simple value
-    #[error("invalid simple value simple value cannot be between 20-32")]
     InvalidSimple,
     /// Error converting to a required integer
-    #[error(transparent)]
-    FromInt(#[from] std::num::TryFromIntError),
+    FromInt(TryFromIntError),
     /// Not well formed data
-    #[error("not well formed data")]
     NotWellFormed(String),
     /// Invalid break stop position
-    #[error("break stop position is invalid")]
     InvalidBreakStop,
 }
+
+impl From<FromUtf8Error> for Error {
+    fn from(value: FromUtf8Error) -> Self {
+        Self::FromUtf8(value)
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(value: TryFromIntError) -> Self {
+        Self::FromInt(value)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Incomplete => write!(f, "incomplete CBOR bytes"),
+            Self::FromUtf8(internal_err) => internal_err.fmt(f),
+            Self::IncompleteIndefinite => write!(f, "incomplete indefinite length data"),
+            Self::InvalidSimple => {
+                write!(
+                    f,
+                    "invalid simple value simple value cannot be between 20-32"
+                )
+            }
+            Self::FromInt(internal_err) => internal_err.fmt(f),
+            Self::NotWellFormed(internal_message) => {
+                write!(f, "not well formed data : {internal_message}")
+            }
+            Self::InvalidBreakStop => write!(f, "break stop position is invalid"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
